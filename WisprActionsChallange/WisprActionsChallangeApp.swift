f989ce6b590f8065c5d8bf834cc8497a1577d8f6
Romulo Mantovani <romulo_11bitten@hotmail.com>
2026-03-30 10:18:50 -0300

    Initial Commit

---
+++ WisprActionsChallange/WisprActionsChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WisprActionsChallangeApp.swift
+//  WisprActionsChallange
+//
+//  Created by Romulo on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WisprActionsChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WisprActionsChallangeTests/WisprActionsChallangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WisprActionsChallangeTests.swift
+//  WisprActionsChallangeTests
+//
+//  Created by Romulo on 30/03/26.
+//
+
+import Testing
+@testable import WisprActionsChallange
+
+struct WisprActionsChallangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
